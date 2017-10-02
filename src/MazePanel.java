b1ef2f3e17ac0bfd252f424c9c586281dc0af1cd
PercaleMouse <user@localhost>
2017-10-03 00:47:16 +0500

    night commit

--- src/MazePanel.java
+++ src/MazePanel.java
@@ -7,33 +7,33 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.border.LineBorder;
 
-public class MazePanel extends javax.swing.JPanel{
+public class MazePanel extends javax.swing.JPanel {
 
-    boolean enableEdit= true; 
+    boolean enableEdit = true;
 
-    int mazeSize=16;
-    int vectorSize = mazeSize*mazeSize;
+    int mazeSize = 16;
+    int vectorSize = mazeSize * mazeSize;
     JButton[] buttons = new JButton[vectorSize];
-    
-    ImageIcon wallIcon= new ImageIcon(getClass().getResource("tile_1_2.png"));
-    ImageIcon flourIcon= new ImageIcon(getClass().getResource("tile_4_1.png"));
-    
+
+    ImageIcon wallIcon = new ImageIcon(getClass().getResource("tile_1_2.png"));
+    ImageIcon flourIcon = new ImageIcon(getClass().getResource("tile_4_1.png"));
+
     ActionListener al;
 
     MazePanel() {
         al = (ActionEvent e) -> {
             JButton b = (JButton) e.getSource();
             if (enableEdit) {
-                if (b.getIcon().equals(wallIcon)){
+                if (b.getIcon().equals(wallIcon)) {
                     b.setIcon(flourIcon);
-                }else{
+                } else {
                     b.setIcon(wallIcon);
                 }
             }
             System.out.println(b.getActionCommand());
         };
 
-        setSize(mazeSize*20, mazeSize*20);
+        setSize(mazeSize * 20, mazeSize * 20);
         setLayout(new GridLayout(mazeSize, mazeSize, 0, 0));
         for (int i = 0; i < vectorSize; i++) {
             buttons[i] = new JButton();
@@ -43,22 +43,23 @@ public class MazePanel extends javax.swing.JPanel{
             buttons[i].setActionCommand(String.valueOf(i));
             buttons[i].enableInputMethods(false);
             buttons[i].setBorder(null);
-            
+
             add(buttons[i]);
         }
 
     }
-    public void setEditModeON(){
-        for (int i = 0; i < vectorSize; i++){
-          enableEdit=true;
-          buttons[i].setBorder(new LineBorder(Color.BLACK));
+
+    public void setEditModeON() {
+        for (int i = 0; i < vectorSize; i++) {
+            enableEdit = true;
+            buttons[i].setBorder(new LineBorder(Color.BLACK));
         }
     }
-    
-    public void setEditModeOFF(){
-        for (int i = 0; i < vectorSize; i++){
-            enableEdit=false;
-          buttons[i].setBorder(null);
+
+    public void setEditModeOFF() {
+        for (int i = 0; i < vectorSize; i++) {
+            enableEdit = false;
+            buttons[i].setBorder(null);
         }
     }
 
